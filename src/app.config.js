6bb210e59b95d7935c02420c4b15cd4c816ab01b
zhouboyi <1144188685@qq.com>
2022-06-08 00:24:50 +0800

    搭建Taro框架

---
+++ src/app.config.js
@@ -0,0 +1,13 @@
+export default {
+  pages: [
+    'pages/list/list',
+    'pages/index/index',
+    'pages/cmd/cmd'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
