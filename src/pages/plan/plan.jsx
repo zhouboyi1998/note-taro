72425f693a42d26adc3bbebf2972305feb0c9e5d
zhouboyi <1144188685@qq.com>
2022-06-14 23:28:12 +0800

    添加计划页面

---
+++ src/pages/plan/plan.jsx
@@ -0,0 +1,48 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { AtTimeline } from 'taro-ui'
+
+// 按需引入 Taro UI
+import "taro-ui/dist/style/components/timeline.scss";
+import "taro-ui/dist/style/components/icon.scss";
+import './plan.scss'
+
+export default class Plan extends Component {
+
+  componentWillMount() {
+    this.state = {}
+  }
+
+  componentDidMount() {
+    let that = this
+    Taro.request({
+      url: 'http://127.0.0.1:18092/note/plan/list',
+      success: function (res) {
+        // 将返回的 JSON 数组赋值给 state 中的 list
+        that.setState({list: res.data})
+      }
+    })
+  }
+
+  componentWillUnmount() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  render() {
+    const list = this.state.list
+    return (
+      <View style='height:100vh'>
+        <AtTimeline
+          items={list}
+        >
+        </AtTimeline>
+      </View>
+    )
+  }
+}
